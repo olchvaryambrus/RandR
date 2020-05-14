1136a88a2b84c50e18fd70e15f1eca7108aade30
oambrus <Ambrus@DESKTOP-9PUL3HJ>
2020-05-14 20:40:54 +0200

    Service és Komponens létrehozás

--- Countries/src/app/app-routing.module.ts
+++ Countries/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountryListComponent } from './country-list/country-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/countries', component: CountryListComponent },
+  { path: '', redirectTo: '/countries', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
